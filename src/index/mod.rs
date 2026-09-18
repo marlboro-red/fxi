26204b8fff03de761007e00a971276409fcc934f
marlboro-red <chanjun.park@wisetechglobal.com>
2026-09-18 13:51:32 +1000

    style: order index modules consistently

--- src/index/mod.rs
+++ src/index/mod.rs
@@ -51,8 +51,8 @@ pub mod segment_io;
 pub(crate) mod source_pack;
 pub(crate) mod source_positions;
 pub mod stats;
-pub mod types;
 pub(crate) mod token_dictionary;
+pub mod types;
 pub mod writer;
 
 // Re-exports for public API
